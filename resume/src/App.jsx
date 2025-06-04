c1a5b02c970830ecf1d6f9811cd40d69a58c8d58
Aman-Raj-Singh-106 <uchihaitachipc@gmail.com>
2025-06-04 10:44:24 +0530

    first commit

---
+++ resume/src/App.jsx
@@ -0,0 +1,10 @@
+import Resume from "./resume";
+
+function App() {
+  return (
+    <div className="App">
+     <Resume />
+    </div>
+  );
+}
+export default App;
---
+++ resume/src/resume.jsx
@@ -0,0 +1,55 @@
+function Resume() {
+    return (
+        <div className="Resume">
+            <h2>Aman Raj Singh</h2>
+
+            <div className="summary">
+                <h3>Summary</h3>
+                <p>Enthusiastic and detail-oriented software developer with a passion for building scalable web applications. Proficient in JavaScript, React, and Node.js, with a strong foundation in computer science principles. Eager to contribute to innovative projects and collaborate with cross-functional teams.</p>
+            </div>
+            <hr />
+            <div className="education">
+                <h3>Education</h3>
+                <p><strong>Currently enrolled in : Diploma in Computer Science Engineering</strong></p>
+                <p>Arka Jain University, 2023 - 2026</p>
+                <p><strong>Passed 10 from Sri Krishna Public School</strong></p>
+                <p>Jamshedpur, 2023</p>
+            </div>
+            <hr />
+            <div className="skills">
+                <h3>Skills</h3>
+                <ul>
+                    <li>Programming Languages: JavaScript, Python, C++</li>
+                    <li>Web Technologies: HTML, CSS, React.js, Node.js</li>
+                    <li>Databases: MongoDB, MySQL</li>
+                    <li>Tools & Platforms: Git, Docker, AWS</li>
+                    <li>Soft Skills: Problem-solving, Team collaboration, Communication</li>
+                </ul>
+            </div>
+            <hr />
+            <div className="projects">
+                <h3>Projects</h3>
+                <ul>
+                    <li><strong>Portfolio Website</strong> - Developed a personal portfolio website using React.js to showcase projects and skills.</li>
+                    <li><strong>E-commerce Website</strong> - Built a Frontend part of an e-commerce platform using HTML,CSS,JAVASCRIPT, implementing user authentication and payment integration.</li>
+                    <li><strong>Fir Filing Website</strong> - Built a Frontend part of an Fir Filing platform using HTML,CSS,JAVASCRIPT, implementing user authentication and real time Fir tracking.</li>
+
+                </ul>
+            </div>
+            <div className="experience">
+                <h3>Experience</h3>
+                <ul>
+                    <li><strong>Internship at Ardent Computech Pvt. Ltd.</strong> - Assisted in developing a web application using HTML,CSS,JAVASCRIPT, contributing to frontend development.</li>
+                </ul>
+            </div>
+            <hr />
+            <div className="contact-info">
+                <span id="contact">Email: <a href="mailto:aman.rajsingh.1874@gmail.com">aman.rajsingh.1874@gmail.com</a></span>
+                <span id="contact">Phone: <a href="tel:+916206505309">+91 62065 05309</a></span>
+                <span id="contact">LinkedIn: <a href="https://www.linkedin.com/in/aman-raj-singh-511830309/">linkedin.com/in/aman-raj-singh</a></span>
+            </div>
+            <hr />
+        </div>
+    )
+}
+export default Resume;
